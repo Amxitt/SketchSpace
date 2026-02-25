c67ba0c4480623ab5467a19f35856ff8a3ceae35
Amxitt <amxittt@gmail.com>
2026-02-25 12:25:33 +0000

    connected ws layer to fe

--- packages/db/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-export declare const prisma: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-export * from "@prisma/client";
-//# sourceMappingURL=index.d.ts.map
